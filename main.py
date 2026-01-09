722d229b6722c24713ac4b408bb200f84d58f1bb
Hiroaki Suzuki <rirazou05410@gmail.com>
2026-01-10 07:50:40 +0900

    Gemini APIでGoogle検索のサンプルを作成

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from google import genai
+from google.genai import types
+
+client = genai.Client()
 
 
-def main():
-    client = genai.Client()
-
+def generate_monster_summary():
+    # Geminiへのリクエスト
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
-        contents="Explain how AI works in a few words",
+        contents="漫画のMonsterの概要について100文字程度で教えて下さい",
     )
 
+    print("=" * 80)
     print(response.text)
+    print("=" * 80)
+
+
+def generate_google_search():
+    # GeminiのWeb検索
+    grounding_tool = types.Tool(google_search=types.GoogleSearch())
+    config = types.GenerateContentConfig(tools=[grounding_tool])
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents="Geminiの最新の状況を教えて下さい",
+        config=config,
+    )
+
+    print("=" * 80)
+    print(response.text)
+    print("=" * 80)
 
 
 if __name__ == "__main__":
-    main()
+    generate_monster_summary()
+    generate_google_search()
